508882c5afe47b6abfe424754ac9b79074323d5c
bluestar7589 <bluestar7589@yahoo.com>
2024-07-12 10:26:30 -0700

    Added tested code.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Advisors mathAdvisor = new Advisors()
+            {
+                FullName = "Jane Doe",
+                Email = "jane.doe@advisor.cptc.edu",
+                OfficeLocation = "Building 17, Room 205"
+            };
+
+            Students student = new Students() 
+            { 
+                FirstName = "John",
+                LastName = "Doe",
+                SchoolEmail = "john.doe@students.cptc.edu",
+                PhoneNumber = "253-555-1234",
+                DateOfBirth = new DateTime(1990, 1, 1),
+                AssignedAdvisor= mathAdvisor
+            };
+
+            Console.WriteLine($"Student: {student.FirstName} has {student.AssignedAdvisor.FullName}");
         }
     }
 }
